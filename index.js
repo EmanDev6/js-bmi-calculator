59b58995f71bd74f8585136c3a8d073f26379ff1
eman <emanali31183@gmail.com>
2025-04-09 04:33:37 +0500

    its js base bmi calculator

---
+++ index.js
@@ -0,0 +1,12 @@
+weight = prompt("Enter Your Weight in kg")
+height = prompt("Enter Your Height in meters")
+
+bmi = weight / (height * height)
+// body mass index (bmi) formula
+
+alert("Your BMI is :" + bmi)
+
+document.write("Your BMI is :" + bmi)
+console.log("Your BMI is :" + bmi)
+
+qqqq
